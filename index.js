c97c82c9888a49523833c968415e3bd05ade6c39
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-02-19 11:43:18 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div')
+}
+
+function increaseRankBy(n) {
+  let lis = document.querySelectorAll('.ranked-list li')
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+  }
+}
